23f51ea04fc4e48a88f7e69321a1b109963c0402
Ibnu Putra <azxarblog@gmail.com>
2023-08-28 00:42:22 +0700

    added profile card components to module alpha

---
+++ src/modules/alpha/components/ProfileCard/index.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const ProfileCard = () => {
+  return (
+    <View>
+      <Text>ProfileCard</Text>
+      <Text>Age</Text>
+      <Text>Point</Text>
+    </View>
+  );
+};
+
+export default ProfileCard;
