eb267eb4e5c3e4e5ff6a7aba912f78e7805fbf94
Ibnu Putra <azxarblog@gmail.com>
2023-08-29 16:26:58 +0700

    added loading to module B

--- src/modules/beta/screens/UserList/index.tsx
+++ src/modules/beta/screens/UserList/index.tsx
@@ -7,9 +7,11 @@ import {USER_GET_ALL} from '../../../../api/api';
 import {API_METHOD, API_STATES} from '../../../../constants';
 import {useFocusEffect} from '@react-navigation/native';
 import {Card} from '../../components';
+import {Spinner} from '../../../../components';
 
 const BetaUserListScreen = ({navigtaion}: ApplicationScreenProps) => {
   const [users, setUsers] = React.useState<any>();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
   // fetch api on screen focused || on navigation load
   useFocusEffect(
     React.useCallback(() => {
@@ -21,10 +23,12 @@ const BetaUserListScreen = ({navigtaion}: ApplicationScreenProps) => {
 
   // Get all user
   const getAllUser = async () => {
+    setIsLoading(!isLoading);
     const {state, data, error} = await fetchAPI(USER_GET_ALL, API_METHOD.GET);
 
     if (state == API_STATES.SUCCESS) {
       setUsers(data);
+      setIsLoading(!!isLoading);
     }
   };
 
@@ -36,12 +40,16 @@ const BetaUserListScreen = ({navigtaion}: ApplicationScreenProps) => {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        contentContainerStyle={styles.userList}
-        data={users}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item, index}) => <Card.UserListCard data={item} />}
-      />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <FlatList
+          contentContainerStyle={styles.userList}
+          data={users}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item, index}) => <Card.UserListCard data={item} />}
+        />
+      )}
     </View>
   );
 };
